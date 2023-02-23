c84cbb2c1f3a8a3de80bcf8d7ba1a111d9246813
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-02-23 22:10:14 +0000

    cards generated from data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Header } from "./components/Header.jsx"
 import { Card } from "./components/Card.jsx"
+import cards from "./data.js"
 
 function App() {
     return (
         <>
             <Header />
             <div>
-                <Card />
+                {cards.map((data, i) => <Card key={i} {...data} />)}
             </div>
         </>
     )
